11dbfe5eb57de950043e509d7219f9d64ce05f24
Twest19 <67450649+Twest19@users.noreply.github.com>
2021-05-19 16:53:29 -0500

    Add files via upload

---
+++ mad_libs.py
@@ -0,0 +1,29 @@
+# We Didn't Start the Fire By Billy Joel
+# But it's a mad lib
+
+person = input("Enter a persons name: ")
+baseball_player = input("Enter a baseball players name: ")
+president_1 = input("Enter a presidents name: ")
+country = input("Enter a country: ")
+book = input("Enter a book title: ")
+thing = input("Enter a thing: ")
+goodbye = input("Enter another word for goodbye: ")
+adjective = input("Enter an adjective: ")
+noun = input("Enter a different noun: ")
+verb = input("Enter a verb: ")
+
+
+print(f"Harry Truman, {person}, Red China, Johnnie Ray")
+print(f"South Pacific, Walter Winchell, {baseball_player}")
+print(f"Joe McCarthy, {president_1}, Studebaker, television")
+print(f"{country}, South Korea, Marilyn Monroe\n")
+
+print("Rosenbergs, H-bomb, Sugar Ray, Panmunjom")
+print(f"Brando, \"The King and I\", and \"{book}\"")
+print(f"Eisenhower, Vaccine, England's got a new {thing}")
+print(f"Marciano, Liberace, Santayana, {goodbye}\n")
+
+print(f"We didn't start the {noun}")
+print(f"It was always {adjective}, since the world's been turning")
+print(f"We didn't start the {noun}")
+print(f"No, we didn't light it, but we tried to {verb} it\n")
