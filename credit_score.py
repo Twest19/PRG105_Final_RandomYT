276b59a4a74c35dd576f4544e36cd5dd939d33b8
Twest19 <67450649+Twest19@users.noreply.github.com>
2021-05-26 22:44:01 -0500

    Add files via upload

---
+++ credit_score.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+
+user_score = int(input("What is your credit score? "))
+
+if user_score >= 720:
+	print(f"A credit score of {user_score} is an excellent credit score.")
+elif 690 <= user_score < 720:
+	print(f"A credit score of {user_score} is a good credit score.")
+elif 630 <= user_score < 690:
+	print(f"A credit score of {user_score} is a fair credit score.")
+else:
+	print(f"A credit score of {user_score} is a bad credit score.")
