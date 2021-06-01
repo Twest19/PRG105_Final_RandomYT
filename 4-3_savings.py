968ec7c08609daa0d42fb68e06528abfcb79fd1a
Twest19 <67450649+Twest19@users.noreply.github.com>
2021-06-01 16:30:45 -0500

    Update 4-3_savings.py

--- 4-3_savings.py
+++ 4-3_savings.py
@@ -1,9 +1,9 @@
 """
-	Write a program that projects yearly income, the amount saved towards retirement each year, and total retirement
-	savings.  Assume a 3 percent raise on the starting income each year, and a 6% yearly return on investment. You will
-	need to ask the user their current age, at what age they want to retire, what their current income is, what percent
-	of their income they save each year, and how much they currently have in savings. ou will calculate how many years
-	until retirement, and display the projected income, savings contribution, and total savings each year.
+    Write a program that projects yearly income, the amount saved towards retirement each year, and total retirement
+    savings.  Assume a 3 percent raise on the starting income each year, and a 6% yearly return on investment. You will
+    need to ask the user their current age, at what age they want to retire, what their current income is, what percent
+    of their income they save each year, and how much they currently have in savings. ou will calculate how many years
+    until retirement, and display the projected income, savings contribution, and total savings each year.
 """
 
 # Ask user for the appropriate information
@@ -29,8 +29,8 @@ print(f"\n{'Year'}{'Income':>12}{'Savings Contribution':>26}{'Total Savings':>19
 
 # Create a for loop to cycle through correct amount of years until retirement and place data accordingly
 for year in range(1, years + 1):
-	print(f"{year:>4}{income:>12,.0f}{contribution:>26,.0f}{total_save:>19,.0f}")
-	income = income * 1.03
-	contribution = income * save_year
-	total_save *= 1.06
-	total_save += contribution
+    print(f"{year:>4}{income:>12,.0f}{contribution:>26,.0f}{total_save:>19,.0f}")
+    income = income * 1.03
+    contribution = income * save_year
+    total_save *= 1.06
+    total_save += contribution
