6e3c10cc41a8dff227f80619283942a59b296a49
Twest19 <67450649+Twest19@users.noreply.github.com>
2021-06-01 16:16:45 -0500

    Update 4-2_song.py

--- 4-2_song.py
+++ 4-2_song.py
@@ -6,9 +6,20 @@
 bottles = 99
 
 while bottles > 0:
-    print(f""
-          f"{bottles} bottles of beer on the wall, "
-          f"\n{bottles} bottles of beer"
-          f"\nTake one down, pass it around")
+    if bottles == 1:
+        print(f""
+              f"{bottles} bottle of beer on the wall, "
+              f"\n{bottles} bottle of beer"
+              f"\nTake one down, pass it around")
+    else:
+        print(f""
+              f"{bottles} bottles of beer on the wall, "
+              f"\n{bottles} bottles of beer"
+              f"\nTake one down, pass it around")
+    
     bottles -= 1
-    print(f"{bottles} bottles of beer on the wall\n")
+
+    if bottles == 1:
+        print(f"{bottles} bottle of beer on the wall\n")
+    else:
+        print(f"{bottles} bottles of beer on the wall\n")
