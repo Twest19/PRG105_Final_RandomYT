2b29a96d65febbf83e2df78e67e3ef69cdda6312
Twest19 <67450649+Twest19@users.noreply.github.com>
2021-06-17 23:08:21 -0500

    Add files via upload

--- 8-3_Assessment.py
+++ 8-3_Assessment.py
@@ -8,31 +8,44 @@
 
 
 def main():
-    file_one = open('accounts.txt', 'r')
-    lines = file_one.readlines()
+    stop = False
     accounts = []
-    for line in lines:
-        x = line.split()
-        accounts.append(x)
-    file_one.close()
+    file_a = 'accounts.txt'
+    try:
+        file_one = open(file_a, 'r')
+    except IOError:
+        print("Sorry the file", file_a, "does not exist.")
+        stop = True
+    else:
+        lines = file_one.readlines()
+        for line in lines:
+            x = line.split()
+            accounts.append(x)
+        file_one.close()
 
-    file_two = open('over90.txt', 'r')
-    num = file_two.readlines()
     over = []
-    for n in num:
-        y = n.split()
-        over.append(y)
-    file_two.close()
-
-    print(f"The following accounts are at least 90 days over due:\n")
-    for x in range(0, len(over)):  # This will get index value for list over
-        for y in range(0, len(accounts)):  # This will get index value for list accounts
-            try:
-                count = accounts[y][0][:-1]
-                if count == over[x][0]:
-                    print(' '.join(accounts[y]))
-            except IndexError:
-                pass
+    file_b = 'over90.txt'
+    try:
+        file_two = open(file_b, 'r')
+    except IOError:
+        print("Sorry the file", file_b, "does not exist")
+        stop = True
+    else:
+        num = file_two.readlines()
+        for n in num:
+            y = n.split()
+            over.append(y)
+        file_two.close()
+    if stop is False:  # This code will only run if the correct file is found
+        print(f"The following accounts are at least 90 days over due:\n")
+        for x in range(0, len(over)):  # This will get index value for list over
+            for y in range(0, len(accounts)):  # This will get index value for list accounts
+                try:
+                    count = accounts[y][0][:-1]
+                    if count == over[x][0]:
+                        print(' '.join(accounts[y]))
+                except IndexError:
+                    pass
 
 
 main()
