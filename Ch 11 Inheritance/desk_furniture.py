3a5d663d2eef4fe411701ade08522d19c3771e9d
Twest19 <67450649+Twest19@users.noreply.github.com>
2021-07-05 19:42:24 -0500

    Add files via upload

---
+++ Ch 11 Inheritance/desk_furniture.py
@@ -0,0 +1,10 @@
+import office_furniture
+
+
+def main():
+
+    my_desk = office_furniture.Desk('Desk', 'metal', '46 inch', '24 inch', '31 inch', '400', 'both', '2')
+    print(my_desk)
+
+
+main()
